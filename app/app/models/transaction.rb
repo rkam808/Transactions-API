8c2e8fb7bcbba5001d9fc3b3dbe0703d9fa9e606
Ryan Kam <rkam0725@gmail.com>
2026-02-27 15:34:43 +0900

    assure 0 amount Transactions are not processed

--- app/app/models/transaction.rb
+++ app/app/models/transaction.rb
@@ -1,5 +1,5 @@
 class Transaction < ApplicationRecord
   belongs_to :user
-  validates :amount, presence: true
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :description, presence: true
 end
